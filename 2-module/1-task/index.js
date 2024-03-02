3dfe768444cda34895a6fe42517ede6a69ffee43
diomid.tolkachev <diomid.tolkachev@rockdevels.com>
2024-03-02 12:26:54 +0300

    2.1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let salariesKey in salaries) {
+    if ((typeof (salaries[salariesKey]) === 'number') && isFinite(salaries[salariesKey])) {
+      result += salaries[salariesKey];
+    }
+  }
+  return result
 }
