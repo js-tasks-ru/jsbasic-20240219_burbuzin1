1eb80ce0545f33d4e7ab77bae97291f963a94ec2
diomid.tolkachev <diomid.tolkachev@rockdevels.com>
2024-03-06 22:11:39 +0300

    3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,7 +1,4 @@
 function camelize(str) {
   return str.split('-')
-    .map((word, index) => index == 0 ? word : word[0]
-    .toUpperCase() + word
-    .slice(1))
-    .join('');
+    .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)).join('');
 }
