b02fad4ea82c24a80cf94b981879a7b982c287d6
diomid.tolkachev <diomid.tolkachev@rockdevels.com>
2024-03-17 19:14:54 +0300

    4.3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i].cells;
+
+    for (let cell of row) {
+      let availableAttr = cell.getAttribute('data-available');
+      if (!table.rows[i].cells[3].hasAttribute('data-available')) {
+        table.rows[i].setAttribute("hidden", "true");
+      }
+
+      switch (availableAttr) {
+        case 'true':
+          table.rows[i].classList.add('available');
+          break;
+        case 'false':
+          table.rows[i].classList.add('unavailable');
+          break;
+      }
+
+      if (cell.innerText == "m") {
+        table.rows[i].classList.add('male');
+      }
+      if (cell.innerText == "f") {
+        table.rows[i].classList.add('female');
+      }
+      if (isFinite(cell.innerText) && cell.innerText < 18) {
+        table.rows[i].style.textDecoration = "line-through";
+      }
+    }
+  }
 }
